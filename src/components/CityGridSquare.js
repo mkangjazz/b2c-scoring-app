d7959ca639e6004ee282450a46f39cbb373a67e0
Mike Kang Jazz <kaizenmike@gmail.com>
2020-05-04 17:23:17 -0500

    reverse white icons

--- src/components/CityGridSquare.js
+++ src/components/CityGridSquare.js
@@ -2,10 +2,29 @@ import React from 'react';
 import utility from '../js/utility';
 
 function CityGridSquare(props) {
+	let imgSrc = '';
+	
+	if (props.type) {
+		imgSrc += props.type;
+	}
+
+	if (props.typeSpecial) {
+		imgSrc += `-${props.typeSpecial}`;
+	}
+
 	return (
 		<li>
-			<button onClick={(e) => props.showSelectTileModal(e)} type="button" data-tile-type-special={props.typeSpecial} data-tile-token={props.type} data-number={props.number} data-city={props.city} >
-				{props.type ? utility.getNameFromToken(props.type) : "None Specified"} {props.typeSpecial ? `${utility.getNameFromToken(props.typeSpecial)}` : "" }
+			<button 
+				className={props.type}
+				onClick={(e) => props.showSelectTileModal(e)}
+				type="button" 
+				data-tile-type-special={props.typeSpecial} 
+				data-tile-token={props.type}
+				data-number={props.number} data-city={props.city}
+			>
+				{imgSrc !== '' ? <img alt={`icon`} className="image-tile-icon" src={`/img/icon-${imgSrc}-white.png`} /> : ''}
+
+				{props.type ? <span>{utility.getNameFromToken(props.type)}</span> : "—"} {props.typeSpecial ? `${utility.getNameFromToken(props.typeSpecial)}` : "" }
 			</button>
 		</li>
 	);
