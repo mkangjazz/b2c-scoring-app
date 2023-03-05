da8036bc2a58a5e29a906921665f812f5c97ad10
Mike Kang Jazz <kaizenmike@gmail.com>
2023-03-05 12:30:41 -0600

    Updates

---
+++ precache-manifest.411d29bdf0f809cf0ce2f9521b437a9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ff9d452fa9615b4263d353212eaa64",
+    "url": "/b2c-scoring-app/index.html"
+  },
+  {
+    "revision": "c9182695875723ad683a",
+    "url": "/b2c-scoring-app/static/css/main.237695af.chunk.css"
+  },
+  {
+    "revision": "ae61ebaa6c3d1bd2cc50",
+    "url": "/b2c-scoring-app/static/js/2.8ed12980.chunk.js"
+  },
+  {
+    "revision": "c9182695875723ad683a",
+    "url": "/b2c-scoring-app/static/js/main.4d053edd.chunk.js"
+  },
+  {
+    "revision": "4c510cdaf42cb5e26508",
+    "url": "/b2c-scoring-app/static/js/runtime~main.325a5b79.js"
+  }
+]);
